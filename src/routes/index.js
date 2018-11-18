d59d34791c24e6bb75838246bf63f789a9af5eeb
Loppik <shantyr_qq@mail.ru>
2018-11-18 14:35:40 +0300

    added guest access

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,12 +7,14 @@ app.use('/admin', require('./admin'));
 
 app.use((req, res, next) => {
   const accessToken = req.get('Authorization');
-  jwt.verify(accessToken, config.secret, (err, decoded) => { // create verify in jwtService
-    if (err) {
-      res.status(401).send({ err:  err.name })
-    }
-    req.body.userId = decoded.userId;
-  })
+  if (accessToken) {
+    jwt.verify(accessToken, config.secret, (err, decoded) => { // create verify in jwtService
+      if (err) {
+        res.status(401).send({ err:  err.name })
+      }
+      req.body.userId = decoded.userId;
+    })
+  }
   next();
 });
 
