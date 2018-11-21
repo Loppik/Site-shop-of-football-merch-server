ea3602c8938650659de643769e96e886a25641e4
Loppik <shantyr_qq@mail.ru>
2018-11-22 00:10:14 +0300

    added ability adding shoes

--- src/routes/shoes.js
+++ src/routes/shoes.js
@@ -1,52 +1,66 @@
 const app = require('express')();
-const { getAllShoesOfOneType, getShoesById } = require('../actions/shoes');
+const { getAllShoesOfOneType, getShoesById, addShoes } = require('../actions/shoes');
 
 app.get('/fb', (req, res) => {
-    getAllShoesOfOneType('FootballBoots')
-        .then((shoes) => {
-            res.send({
-                shoes: shoes.shoes,
-            })
-        })
-        .catch((err) => {
-            res.send({msg: err, err: 'No football boots'})
-        });
+  getAllShoesOfOneType('FootballBoots')
+    .then((shoes) => {
+      res.send({
+        shoes: shoes.shoes,
+      })
+    })
+    .catch((err) => {
+      res.send({ msg: err, err: 'No football boots' })
+    });
 });
 
 app.get('/centipedes', (req, res) => {
-    getAllShoesOfOneType('Сentipedes')
-        .then((shoes) => {
-            res.send({
-                shoes: shoes.shoes,
-            })
-        })
-        .catch((err) => {
-            res.send({msg: err, err: 'No centipedes'})
-        });
+  getAllShoesOfOneType('Сentipedes')
+    .then((shoes) => {
+      res.send({
+        shoes: shoes.shoes,
+      })
+    })
+    .catch((err) => {
+      res.send({ msg: err, err: 'No centipedes' })
+    });
 });
 
 app.get('/forRun', (req, res) => {
-    getAllShoesOfOneType('ForRun')
-        .then((shoes) => {
-            res.send({
-                shoes: shoes.shoes,
-            })
-        })
-        .catch((err) => {
-            res.send({msg: err, err: 'No shoes for run'})
-        });
+  getAllShoesOfOneType('ForRun')
+    .then((shoes) => {
+      res.send({
+        shoes: shoes.shoes,
+      })
+    })
+    .catch((err) => {
+      res.send({ msg: err, err: 'No shoes for run' })
+    });
 });
 
 app.get('/:id', (req, res) => {
-    getShoesById(req.params.id)
-        .then((shoes) => {
-            res.send({
-                shoes: shoes.shoes
-            })
-        })
-        .catch((err) => {
-            res.send({err})
-        })
+  getShoesById(req.params.id)
+    .then((shoes) => {
+      res.send({
+        shoes: shoes.shoes
+      })
+    })
+    .catch((err) => {
+      res.send({ err })
+    })
+})
+
+app.post('/', (req, res) => {
+  addShoes(req.body)
+    .then((shoes) => {
+      res.send({})
+    })
+    .catch((err) => {
+      res.status(500).send({ err })
+    })
+})
+
+app.delete('/', (req, res) => {
+  
 })
 
 
