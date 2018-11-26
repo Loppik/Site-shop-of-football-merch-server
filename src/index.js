33e84647104398c8a4cdf2ba0041fe6dd9853680
Loppik <shantyr_qq@mail.ru>
2018-11-26 21:16:51 +0300

    deleted useless files and added export app in main server file

--- src/index.js
+++ src/index.js
@@ -3,26 +3,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const mongoose = require('./mongoose');
-const passport = require('passport');
-
-const LocalStrategy = require('passport-local');
-const JwtStrategy = require('passport-jwt').Strategy; 
-const ExtractJwt = require('passport-jwt').ExtractJwt; 
-const jwtsecret = 'mysecretkey';
 
 const config = require('../config');
 
 const app = express();
 
-const User = require('./schemes/userSchema');
-const Shoes = require('./schemes/shoesSchema');
-const Review = require('./schemes/reviewSchema');
-
 const routes = require('./modules/index');
 
 //app.use(errorHandler);
-
 
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -36,5 +24,4 @@ app.listen(config.port, () => {
   console.log(`listening on http://${config.host}:${config.port}`);
 });
 
-
-
+module.exports = app;
