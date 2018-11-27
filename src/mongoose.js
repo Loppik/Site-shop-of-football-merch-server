ed1c20cffb790e4b3fb623c6623a2b56024b179f
Loppik <shantyr_qq@mail.ru>
2018-11-27 22:38:11 +0300

    set new url parser for mongoose

--- src/mongoose.js
+++ src/mongoose.js
@@ -4,9 +4,7 @@ const config = require('../config');
 mongoose.Promise = Promise;
 mongoose.set('debug', config.debug);
 
-console.log('mongodb://' + config.database);
-
-mongoose.connect('mongodb://' + config.database, (err) => {
+mongoose.connect('mongodb://' + config.database, { useNewUrlParser: true }, (err) => {
     
 });
 module.exports = mongoose;
