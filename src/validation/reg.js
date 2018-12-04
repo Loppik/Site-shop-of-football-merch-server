eed3938a63bcea2ce0c2fd75eb00e56e4a9491a6
Loppik <shantyr_qq@mail.ru>
2018-12-04 16:29:33 +0300

    validation changed

--- src/validation/reg.js
+++ src/validation/reg.js
@@ -1,31 +1,53 @@
-const {isInvalidLogin, isInvalidPassword, isInvalidPhoneNumber, isInvalidName, isInvalidEmail, isInvalidAddress} = require('./validation');
+const { isInvalidLogin, isInvalidPassword, isInvalidPhoneNumber, isInvalidName, isInvalidEmail, isInvalidAddress } = require('./validation');
+
+const areLoginObjectFieldsAvailable = (data) => {
+  if (!data.hasOwnProperty('login')) {
+    return 'no login field';
+  }
+  if (!data.hasOwnProperty('password')) {
+    return 'no password field';
+  }
+  if (!data.hasOwnProperty('phoneNumber')) {
+    return 'no phone number field';
+  }
+  if (!data.hasOwnProperty('name')) {
+    return 'no name field';
+  }
+  if (!data.hasOwnProperty('email')) {
+    return 'no email field';
+  }
+  if (!data.hasOwnProperty('address')) {
+    return 'no address field';
+  }
+  return false;
+}
 
 const isInvalidRegData = (data) => {
-    return new Promise((resp, rej) => {
-        let err = isInvalidLogin(data);
-        if (err) rej(err);
-        
-        err = isInvalidPassword(data);
-        if (err) rej(err);
+  return new Promise((resp, rej) => {
+    let err = isInvalidLogin(data);
+    if (err) rej(err);
 
-        err = isInvalidPhoneNumber(data);
-        if (err) rej(err);
+    err = isInvalidPassword(data);
+    if (err) rej(err);
 
-        
-        err = isInvalidName(data);
-        if (err) rej(err);
+    err = isInvalidPhoneNumber(data);
+    if (err) rej(err);
 
 
-        err = isInvalidEmail(data);
-        if (err) rej(err);
+    err = isInvalidName(data);
+    if (err) rej(err);
 
-        err = isInvalidAddress(data);
-        if (err) rej(err);
 
-        return resp(false);
-    });
+    err = isInvalidEmail(data);
+    if (err) rej(err);
+
+    err = isInvalidAddress(data);
+    if (err) rej(err);
+
+    return resp(false);
+  });
 };
 
 module.exports = {
-    isInvalidRegData
+  isInvalidRegData
 }
--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,91 +1,67 @@
 const isInvalidLogin = (data) => {
-    if (data.hasOwnProperty('login')) {
-        if (data.login.length < 3 || data.login.length > 15) {
-            return 'incorrect login length';
-        }
-        const regexp = new RegExp('^[a-zA-Z0-9_]*$');
-        if (data.login.search(regexp) == -1) {
-            return 'incorrect symbol in login';
-        }
-    } else {
-        return 'no login';
-    }
-    return false;
+  if (data.login.length < 3 || data.login.length > 15) {
+    return 'incorrect login length';
+  }
+  const regexp = new RegExp('^[a-zA-Z0-9_]*$');
+  if (data.login.search(regexp) == -1) {
+    return 'incorrect symbol in login';
+  }
+  return false;
 }
 
 const isInvalidPassword = (data) => {
-    if (data.hasOwnProperty('password')) {
-        if (data.password.length < 3 || data.password.length > 15) {
-            return 'incorrect password length';
-        }
-        const regexp = new RegExp('^[a-zA-Z0-9_]*$');
-        if (data.password.search(regexp) == -1) {
-            return 'incorrect symbol in password';
-        }
-    } else {
-        return 'no password';
-    }
-    return false;
+  if (data.password.length < 3 || data.password.length > 15) {
+    return 'incorrect password length';
+  }
+  const regexp = new RegExp('^[a-zA-Z0-9_]*$');
+  if (data.password.search(regexp) == -1) {
+    return 'incorrect symbol in password';
+  }
+  return false;
 }
 
 const isInvalidPhoneNumber = (data) => {
-    if (data.hasOwnProperty('phoneNumber')) {
-        if (data.phoneNumber.toString().length != 7) {
-            return 'incorrect phone number length';
-        } else {
-            if (isNaN(Number(data.phoneNumber))) {
-                return 'incorrect phone number format';
-            }
-        }
-    } else {
-        return 'no phone number';
+  if (data.phoneNumber.toString().length != 7) {
+    return 'incorrect phone number length';
+  } else {
+    if (isNaN(Number(data.phoneNumber))) {
+      return 'incorrect phone number format';
     }
-    return false;
+  }
+  return false;
 }
 
 const isInvalidName = (data) => {
-    if (data.hasOwnProperty('name')) {
-        if (data.name.length < 3 || data.name.length > 255) {
-            return 'incorrect name length';
-        }
-        const regexp = new RegExp('^[a-zA-Z]*$');
-        if (data.name.search(regexp) == -1) {
-            return 'incorrect symbol in name';
-        }
-    } else {
-        return 'no name';
-    }
-    return false;
+  if (data.name.length < 3 || data.name.length > 255) {
+    return 'incorrect name length';
+  }
+  const regexp = new RegExp('^[a-zA-Z]*$');
+  if (data.name.search(regexp) == -1) {
+    return 'incorrect symbol in name';
+  }
+  return false;
 }
 
 const isInvalidEmail = (data) => {
-    if (data.hasOwnProperty('email')) {
-        const regexp = new RegExp('@');
-        if (data.email.match(regexp) == null) {
-            return 'no symbol @';
-        }
-    } else {
-        return 'no email';
-    }
-    return false;
+  const regexp = new RegExp('@');
+  if (data.email.match(regexp) == null) {
+    return 'no symbol @';
+  }
+  return false;
 }
 
 const isInvalidAddress = (data) => {
-    if (data.hasOwnProperty('address')) {
-        if (data.address.length < 5 || data.address.length > 255) {
-            return 'incorrect address length';
-        }
-    } else {
-        return 'no address';
-    }
-    return false;
-} 
+  if (data.address.length < 5 || data.address.length > 255) {
+    return 'incorrect address length';
+  }
+  return false;
+}
 
 module.exports = {
-    isInvalidLogin,
-    isInvalidPassword,
-    isInvalidPhoneNumber,
-    isInvalidName,
-    isInvalidEmail,
-    isInvalidAddress
+  isInvalidLogin,
+  isInvalidPassword,
+  isInvalidPhoneNumber,
+  isInvalidName,
+  isInvalidEmail,
+  isInvalidAddress
 }
