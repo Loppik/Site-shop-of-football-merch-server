c50ab74aa33c43925a69473502eae89024c2dda2
Loppik <shantyr_qq@mail.ru>
2019-02-23 10:32:18 +0300

    added connection to remote database

--- config.js
+++ config.js
@@ -18,9 +18,19 @@ const test = {
   database: 'localhost/sportwearTest',
 }
 
+const prod = {
+  secret: 'QWERTYUIOPASDFGHJKLZXCVBNM',
+  accessTokenLifetime: '1d',
+  host: 'localhost',
+  port: '8081',
+  debug: false,
+  database: 'Loppik:adihip11@sportwear-shard-00-00-blxsj.mongodb.net:27017,sportwear-shard-00-01-blxsj.mongodb.net:27017,sportwear-shard-00-02-blxsj.mongodb.net:27017/sportwear?ssl=true&replicaSet=sportwear-shard-0&authSource=admin&retryWrites=true',
+}
+
 const config = {
   dev,
-  test
+  test,
+  prod
 };
 
 module.exports = config[env];
