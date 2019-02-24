8f1ae9c9c3c3c8a14c768a96887f0506beba0bc0
Loppik <shantyr_qq@mail.ru>
2019-02-24 12:37:46 +0300

    mongoose error output added

--- src/mongoose.js
+++ src/mongoose.js
@@ -5,6 +5,7 @@ mongoose.Promise = Promise;
 mongoose.set('debug', config.debug);
 
 mongoose.connect('mongodb://' + config.database, { useNewUrlParser: true }, (err) => {
-    
+  console.log(err)
 });
+console.log(`Connect ${config.database}`)
 module.exports = mongoose;
